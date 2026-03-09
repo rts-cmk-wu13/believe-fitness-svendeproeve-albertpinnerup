28597e660f515b56a3e1d46b108d12728c50e99f
Albert Pinnerup <albert.pinnerup@gmail.com>
2026-03-09 11:59:58 +0100

    splashScreen done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import SplashScreen from '@/components/SplashScreen';
 import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans'>
-            <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start'></main>
+        <div className='flex min-h-screen items-center justify-center'>
+            <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between  sm:items-start'>
+                <SplashScreen />
+            </main>
         </div>
     );
 }
--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,43 +1,49 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Button } from './ui/button';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function SplashScreen() {
+    const router = useRouter();
+    const randomizer = Math.floor(Math.random() * 2);
+    console.log(randomizer);
+    const [hasVisited, _setHasVisited] = useState(() => {
+        if (sessionStorage.getItem('hasVisited')) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    const imgUrl = randomizer ? '/splash1.jpg' : '/splash2.png';
+
+    useEffect(() => {
+        if (!hasVisited) {
+            sessionStorage.setItem('hasVisited', `${hasVisited}`);
+        } else {
+            router.push('/dashboard');
+        }
+    }, []);
+
     return (
-        <section className='relative p-6 flex min-h-svh w-full flex-col items-center justify-between gap-6 overflow-hidden font-sans'>
+        <section className='relative z-10 p-6 pb-12 flex min-h-svh w-full flex-col items-center justify-between gap-6 overflow-hidden font-sans'>
             <Image
-                src='splash1.jpg'
+                src={imgUrl}
                 alt='Hero Image'
                 fill={true}
                 className='-z-10 object-cover'
                 priority
             />
 
-            <div className='w-full flex flex-col items-center gap-4 z-10'>
-                <Image src='/assets/landrup_logo.svg' alt='Landrup Logo' width={64} height={64} />
-                <Image
-                    src='/assets/logo_text.svg'
-                    alt='Landrup Text Logo'
-                    className='mr-16 w-full'
-                    width={365}
-                    height={75}
-                    loading='eager'
-                    priority
-                />
+            <div className='w-full flex flex-col mt-auto mb-4 gap-4 z-10'>
+                <h1 className='font-bold text-secondary text-6xl'>Believe Fitness</h1>
+                <h2 className='text-background text-xl'>Train like a pro</h2>
             </div>
-            <Link href='/log-in' className='z-10 text-lg max-w-[240px] w-full mt-auto'>
-                <Button className='z-10 text-lg max-w-[240px] w-full mt-auto'>Log ind</Button>
+            <Link href='/dashboard' className='z-10'>
+                <Button className='z-10 mt-auto'>START TRAINING</Button>
             </Link>
-            <a href='#holdtyper' className='z-10'>
-                <Image
-                    src='/assets/arrow_down.svg'
-                    alt='Scroll Down Indicator'
-                    width={48}
-                    height={48}
-                    className='animate-bounce'
-                    loading='eager'
-                    priority
-                />
-            </a>
         </section>
     );
 }
